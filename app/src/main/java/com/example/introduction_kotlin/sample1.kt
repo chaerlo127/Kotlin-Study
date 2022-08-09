67fe143c6baedd8a78fac5412f28480c0e45e587
chaerlo127 <monynony0203@gmail.com>
2022-08-09 20:12:23 +0900

    FEAT: nullable / Nonnull

--- app/src/main/java/com/example/introduction_kotlin/sample1.kt
+++ app/src/main/java/com/example/introduction_kotlin/sample1.kt
@@ -1,5 +1,7 @@
 package com.example.introduction_kotlin
 
+import java.lang.IllegalArgumentException
+
 fun main(){
     helloWorld()
     println(add(4, 5))
@@ -13,6 +15,11 @@ fun main(){
     println("this is 2\$a") // $를 문자로 사용할 때 필요한 백슬래쉬
 
     checkNumber(1)
+
+    forAndWhile()
+
+//    nullcheck()
+//    ignoreNulls(null)
 }
 
 /**
@@ -105,4 +112,67 @@ fun array(){
     arrayList.add(10)
     arrayList.add(20)
 
+}
+
+/**
+ * 6. 반복문 (for, while)
+ *
+ */
+
+fun forAndWhile(){
+    val students = arrayListOf("a", "b", "c", "d")
+
+    for(name in students){
+        println("${name}")
+    }
+
+    for((index, name) in students.withIndex()){
+        println("${index} 번째 학생: ${name}")
+    }
+
+    var sum : Int = 0
+    for(i in 1..10 step 2){ // step 2씩 띄워서 계산하기 + downTo[10부터 차례로 내려오기] + until 100 [100을 포함 하지 않음 1~99]
+        sum += i
+    }
+    println(sum)
+
+    var index = 0
+    while(index<10){
+        println("current index : ${index}")
+        index++
+    }
+}
+/**
+ * 7. nullable / Nonnull
+ */
+fun nullcheck(){
+    // NPE : Null pointer Exception
+    var name : String = "ari" // nonnull type
+    var nullName : String? = null // ? 를 붙여주면 nullable type string이 됨
+
+    var nameInUpperCase = name.toUpperCase()
+    var nullNameInUpperCase = nullName?.toUpperCase() //-> null인지 아닌지 몰라서, null을 사용해도 되는지 모르겠음
+    // null이면 null로 반환해줌
+
+    // ? : 엘베스 프레슬리 연산자
+
+    val lastName : String? = "null"
+//    val fullName = name + " " + (lastName?: "No lastName")
+    val fullName = lastName ?: throw IllegalArgumentException("No last name")
+    println(fullName)
+
+}
+
+
+// !! : nullable type으로 지정되어 있지만 null이 아닌 것을 나타내는 연산자
+fun ignoreNulls(str : String?){
+    val mNotnull : String = str!! // complier에게 null일리가 없으니까 아니라고 생각하라고 알려줌
+    val upper = mNotnull.toUpperCase()
+
+
+    val email: String ? = "123@gnana.com"
+    email?.let { // email 이 null이 아니면 let으로 실행하라 
+        // 자신의 객체를 lamda 식 내부로 옮겨줌
+        println("my email is ${email}")
+    }
 }
