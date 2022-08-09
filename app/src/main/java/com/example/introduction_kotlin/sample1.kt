99067bdcdb7a13668658eece22413d32b882cb10
chaerlo127 <monynony0203@gmail.com>
2022-08-09 18:21:17 +0900

    first commit

---
+++ app/src/main/java/com/example/introduction_kotlin/sample1.kt
@@ -0,0 +1,17 @@
+package com.example.introduction_kotlin
+
+fun main(){
+    helloWorld()
+}
+
+/**
+ * 1. 함수
+ */
+
+fun helloWorld() : Unit{ // Unit = void
+    println("Hello World!")
+}
+
+fun add(a : Int, b : Int) : Int{ // 변수가 뒤에 온다. Int가 대문자이다.
+
+}
