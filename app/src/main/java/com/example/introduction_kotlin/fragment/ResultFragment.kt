65f610e4ac693bf3dec5ce2ef22a28abad6144e2
chaerlo127 <monynony0203@gmail.com>
2022-08-11 11:02:47 +0900

    FEAT: create result, selection window

--- app/src/main/java/com/example/introduction_kotlin/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/introduction_kotlin/fragment/ResultFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.introduction_kotlin.R
+import kotlinx.android.synthetic.main.fragment_result.*
 
 
 /**
@@ -14,13 +17,49 @@ import com.example.introduction_kotlin.R
  * create an instance of this fragment.
  */
 class ResultFragment : Fragment() {
+// bundle로 가져온 데이터를 잡아줘야한다.
 
+    var option = -1
+    lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        option = arguments?.getInt("index") ?:-1 // null이면 -1을 반환한다.
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        setResult(option)
+        home.setOnClickListener{
+            navController.navigate(R.id.action_resultFragment_to_mainFragment2)
+        }
+    }
+
+    private fun setResult(option: Int) {
+        when(option){
+            1->{
+                result_title.text = "You are a QUITTER"
+//                result_title.setText("")
+                sentence.text = "You can let the person easily."
+            }
+            2->{
+                result_title.text = "You should focus on yourself"
+                sentence.text = "You become really clingy to your ex."
+            }
+            3->{
+                result_title.text = "You should take it easy"
+                sentence.text = "You can do crazy things no matter what it takes."
+            }
+            4->{
+                result_title.text = "You are pretty mature."
+                sentence.text = "You can easily accept the break-up"
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/introduction_kotlin/fragment/SelectionFragment.kt
+++ app/src/main/java/com/example/introduction_kotlin/fragment/SelectionFragment.kt
@@ -5,16 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.introduction_kotlin.R
+import kotlinx.android.synthetic.main.fragment_selection.*
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SelectionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SelectionFragment : Fragment() {
+class SelectionFragment : Fragment() , View.OnClickListener{
     lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +30,42 @@ class SelectionFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         navController = Navigation.findNavController(view)
-        
+        back_btn.setOnClickListener(this)
+        option_1.setOnClickListener(this)
+        option_2.setOnClickListener(this)
+        option_3.setOnClickListener(this)
+        option_4.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View?) {
+        // view 에 있는 id의 값을 불러온다.
+        when(v?.id){
+            R.id.back_btn ->{
+                navController.navigateUp()
+                // 안드로이드는 stack처럼 view를 저장한다.
+                // 앞으로 가기 위해서는 현재 화면을 pop 하면 된다.
+            }
+            R.id.option_1 ->{
+                navigateWithIndex(1)
+            }R.id.option_2 ->{
+                navigateWithIndex(2)
+            }R.id.option_3 ->{
+                navigateWithIndex(3)
+            }R.id.option_4 ->{
+                navigateWithIndex(4)
+            }
+        }
+    }
+
+    private fun navigateWithIndex(index: Int){
+        val bundle = bundleOf("index" to index)
+        // bundle로 쌓아서 데이터를 보내준다.
+        // index라는 key안의 index를 넣어준다.
+        // index(key) to index(value)
+
+        navController.navigate(R.id.action_selectionFragment_to_resultFragment,bundle)
+    }
+
+
 }
